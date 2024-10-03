79df29920abebd326afe1b1f2027da7214e08f13
Shubham Giri <shubhamgiri7631.sg@gmail.com>
2024-10-03 22:56:01 +0530

    started scope

---
+++ Scope, Closure and HOF/Function Recap/index.js
@@ -0,0 +1,12 @@
+function percentage(marks, total) {
+  return (marks * 100) / total;
+}
+
+let percentage = function (marks, total) {
+  return (marks * 100) / total;
+};
+
+let percentage = (marks, total) => {
+  return (marks * 100) / total;
+};
+
